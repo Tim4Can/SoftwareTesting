bd36dcc65e8b5b70767bbd3737d1ddb6726d4a4f
ctt <939708327@qq.com>
2020-06-28 01:58:22 +0800

    reflection

--- src/main/java/com/tongji/test/controller/TestController.java
+++ src/main/java/com/tongji/test/controller/TestController.java
@@ -2,28 +2,31 @@ package com.tongji.test.controller;
 
 import com.tongji.test.service.WhiteBoxService;
 import com.tongji.test.util.ConstantSalePath;
+import com.tongji.test.util.SpringContextUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 
 @CrossOrigin(origins = "*", allowCredentials = "true")
 @RestController
 public class TestController {
-    @Autowired
-    WhiteBoxService whiteBoxService;
-
-    @RequestMapping(value = "/hello", method = RequestMethod.POST)
-    public String hello(@RequestParam(value = "name") String name){
-        return "hello "+name;
-    }
 
     @PostMapping("/test")
-    public List<Object> example(){
-        List<Object> result = whiteBoxService.Run(ConstantSalePath.DCTest);
+    public List<Object> Test() {
+        List<Object> result = null;
+
+        try{
+            Class<?> cls = Class.forName("com.tongji.test.service.WhiteBoxService");
+            Method method = cls.getMethod("Run", String.class);
+            result = (List<Object>)method.invoke(cls.getDeclaredConstructor().newInstance(), ConstantSalePath.DCTest);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return result;
     } 
 
